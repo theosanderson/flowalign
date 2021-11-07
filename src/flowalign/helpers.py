adfefeb0e053c65ee7761356028e0730d5bab2d4
Theo Sanderson <theo@sndrsn.co.uk>
2021-11-07 02:08:32 +0000

    Update helpers.py

--- src/flowalign/helpers.py
+++ src/flowalign/helpers.py
@@ -11,10 +11,10 @@ def get_aligner(reference_filename):
 
  
     opt_a = 1
-    opt_b = 4
-    opt_q = 6
-    opt_q2 =26
-    opt_e = 2
+    opt_b = 19
+    opt_q = 39
+    opt_q2 =81
+    opt_e = 3
     opt_e2 = 1
     opt_sc_ambi = 0
 
@@ -24,7 +24,7 @@ def get_aligner(reference_filename):
 
     scoring = (opt_a, opt_b, opt_q, opt_e, opt_q2, opt_e2, opt_sc_ambi)
     aligner = mp.Aligner(reference_filename,
-                         preset="asm20",
+                         preset="asm5",
                          extra_flags=MM_F_SAM_HIT_ONLY | MM_F_NO_PRINT_2ND,
                          scoring=scoring)  # load or build index
     if not aligner:
